fb15c15eef066955eeebba37aad9f04ea8921569
Abdi-Cheda <abdicheda75@gmail.com>
2024-01-11 09:22:09 +0300

    commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-# Database Setup
 engine = create_engine('sqlite:///database.db')
 Session = sessionmaker(bind=engine)
 session = Session()
