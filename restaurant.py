52234bc98c6e8466f258c402f8dd51c8c6bf98f2
Abdi-Cheda <abdicheda75@gmail.com>
2024-01-11 09:15:03 +0300

    commit

--- restaurant.py
+++ restaurant.py
@@ -9,9 +9,7 @@ class Restaurant(Base):
     reviews = relationship("Review", back_populates="restaurant")
 
     def reviews(self):
-        # This method should have a different name to avoid conflict with the attribute
         return self.reviews
 
     def customers(self):
-        # This method fetches distinct customers who have reviewed the restaurant
         return set(review.customer for review in self.reviews)
